a693934dd7f3be1f14ae344954fe475ac1602623
Tate Jeppesen <jeppesenlelouch@gmail.com>
2017-07-06 14:33:04 -0600

    added before action user_activities

--- app/controllers/api/user_activities_controller.rb
+++ app/controllers/api/user_activities_controller.rb
@@ -1,4 +1,5 @@
 class Api::UserActivitiesController < ApplicationController
+  before_action :set_user_activities, only: :destroy
   def index
     render json: current_user.user_activities.all
   end
