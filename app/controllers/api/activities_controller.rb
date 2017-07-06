7ee057378b3a9c49fd7c06971471d6b2658622e7
Tate Jeppesen <jeppesenlelouch@gmail.com>
2017-07-06 14:18:19 -0600

    activites and user_activities controllers

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -1,2 +1,37 @@
 class Api::ActivitiesController < ApplicationController
+  before_action :set_activity, only: (:update, :destroy)
+
+  def index
+    render json: Activity.all
+  end
+
+  def create
+    activity = activity.new(activity_params)
+    if activity.save
+      render json: activity
+    else
+      render json: { errors: activities.errors.full_messges }, status: :bad_request
+    end
+  end
+
+  def update
+    if @activity.update(activity_params)
+      render json: activity
+    else
+      render json: { errors: @activity.errors.full_messages }, status: :bad_request
+  end
+
+  def destroy
+    @activity.destroy
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :time, :date, :description, :location, :age, :inside, :url)
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
 end
--- app/controllers/api/user_activities_controller.rb
+++ app/controllers/api/user_activities_controller.rb
@@ -1,2 +1,24 @@
 class Api::UserActivitiesController < ApplicationController
+  def index
+    render json: current_user.user_activities.all
+  end
+
+  def create
+    user_activities = current_user.user_activities.new(user_activities)
+    if user_activities.save
+      render json: user_activities
+    else
+      render json: { errors: user_activities.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    @user_activities.destroy
+  end
+
+  private
+
+  def set_user_activities
+    @user_activities = current_user.user_activities.find(params[:id])
+  end
 end
