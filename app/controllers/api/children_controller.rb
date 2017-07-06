1c45d1fd33703716125f0700d3c171a430a48397
Stockton <stocktonhedges@gmail.com>
2017-07-06 14:20:49 -0600

    children and logs controllers

--- app/controllers/api/children_controller.rb
+++ app/controllers/api/children_controller.rb
@@ -1,2 +1,37 @@
 class Api::ChildrenController < ApplicationController
+  before_action :set_child, only: [:update, :destroy]
+
+  def index
+    render json: current_user.children
+  end
+
+  def create
+    child = current_user.children.new(child_params)
+    if children.save
+      render json: child
+    else
+      render json: { errors: child.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    if @child.update(child_params)
+      render json: @child
+    else
+      render json: { errors: @child.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    @child.destroy
+  end
+
+  private
+    def set_child
+      @child = current_user.children.find(params[:id])
+    end
+
+    def child_params
+      params.require(:child).permit(:name, :age, :interest)
+    end
 end
--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -1,2 +1,37 @@
 class Api::LogsController < ApplicationController
+  before_action :set_log, only: [:update, :destroy]
+
+  def index
+    render json: current_user.logs
+  end
+
+  def create
+    log = current_user.logs.new(log_params)
+    if logs.save
+      render json: log
+    else
+      render json: { errors: log.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    if @log.update(log_params)
+      render json: @log
+    else
+      render json: { errors: @log.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    @log.destroy
+  end
+
+  private
+    def set_log
+      @log = current_user.logs.find(params[:id])
+    end
+
+    def log_params
+      params.require(:log).permit(:rate, :experience, :description)
+    end
 end
