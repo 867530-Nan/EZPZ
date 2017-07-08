172bd6cd2da99f331f3dca05a8e355cd4c090ce1
Addy King <ashepherdking@gmail.com>
2017-07-08 14:30:43 -0600

    navbar styles

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Icon, Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { handleLogout } from '../actions/auth';
 import { withRouter } from 'react-router-dom';
+import '../styles/navbar.css'
 
 class NavBar extends Component {
   rightNavs = () => {
@@ -13,40 +14,43 @@ class NavBar extends Component {
       return(
         <Menu.Menu position='right'>
           <Link to='/profile'>
-            <Menu.Item name='Profile' />
+            <Button inverted color='red'>Profile</Button>
           </Link>
           <Link to='/itinerary'>
-            <Menu.Item name='Itinerary' />
+             <Button inverted color='red'>Itinerary</Button>
           </Link>
           <Link to='/activities'>
-            <Menu.Item name='Activities' />
+             <Button inverted color='red'>Activities</Button>
           </Link>
-          <Menu.Item
-            name='Logout'
+          <Button
             onClick={() => dispatch(handleLogout(history))}
-          />
+            inverted color='red'
+          >Logout
+          </Button>  
         </Menu.Menu>
       );
     } else {
       return(
         <Menu.Menu position='right'>
           <Link to='/register'>
-            <Menu.Item name='Register' />
+            <Button inverted color='red'>Register</Button>
           </Link>
           <Link to='/login'>
-            <Menu.Item name='Login' />
+            <Button inverted color='red'>Login</Button>
           </Link>
         </Menu.Menu>
       );
     }
   }
+            
+            
 
   render() {
     return (
-      <div>
+      <div className='mainNavbar'>
         <Menu pointing secondary>
           <Link to='/'>
-            <Menu.Item name='home' />
+            <Button inverted color='red'>Home</Button>
           </Link>
           { this.rightNavs() }
         </Menu>
@@ -54,6 +58,7 @@ class NavBar extends Component {
     )
   }
 }
+           
 
 const mapStateToProps = (state) => {
   return { user: state.user }
