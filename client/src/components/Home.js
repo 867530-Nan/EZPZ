59230121379508fa6a0a26695c174b0d8552548d
Stockton <stocktonhedges@gmail.com>
2017-07-08 15:01:08 -0600

    sidenav login working

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,91 @@
 import React, { Component } from 'react';
-import { Header, Segment, Button, Form, Container } from 'semantic-ui-react';
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Form } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { handleLogin } from '../actions/auth';
+import '../styles/home.css';
 
 class Home extends Component {
+  state = { visible: false, email: '', password: '' }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+  handleChange = (e) => {
+    const { id , value } = e.target;
+    this.setState({ [id]: value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { dispatch, history } = this.props;
+    const { email, password } = this.state;
+
+    dispatch(handleLogin(email, password, history));
+  }
+
   render() {
-    return(
+    const { visible, email, password } = this.state
+    return (
       <div>
-        <Header as='h1' textAlign='center'>Home Component</Header>
-        <Container>
-          <Segment>This is our blurb about things. When we hanging out???</Segment>
-          <Button
-            color='red'
-            content='Like'
-            icon='heart'
-            label={{ basic: true, color: 'red', pointing: 'left', content: '2,048' }}
-          />
-        </Container>
+        <Button className='loginButton' onClick={this.toggleVisibility}>Login!</Button>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='slide out'
+            width='thin'
+            direction='right'
+            visible={visible}
+            icon='labeled'
+            vertical
+            inverted
+          >
+            <Menu.Item name='Login'>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Field>
+                <label>Email</label>
+                <input
+                  autoFocus
+                  required
+                  id='email'
+                  value={email}
+                  placeholder='Email'
+                  onChange={this.handleChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Password</label>
+                <input
+                  required
+                  id='password'
+                  value={password}
+                  placeholder='Password'
+                  type='password'
+                  onChange={this.handleChange}
+                />
+              </Form.Field>
+              <Segment textAlign='center' basic>
+                <Button primary type='submit'>Submit</Button>
+              </Segment>
+            </Form>
+            </Menu.Item>
+            <Menu.Item name='gamepad'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item name='camera'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>Application Content</Header>
+              <Image src='/assets/images/wireframe/paragraph.png' />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
-    );
+    )
   }
 }
 
-export default Home;
+export default connect()(Home);
