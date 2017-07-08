8099b5a1cb2e1a84b2526373dd320c5ae98252c5
Tate Jeppesen <jeppesenlelouch@gmail.com>
2017-07-08 13:13:46 -0600

    modified activities

--- client/src/components/Activities.js
+++ client/src/components/Activities.js
@@ -1,13 +1,44 @@
 import React from 'react'
-import { Header, Form } from 'semantic-ui-react'
+import { Header, Form, Segment, Button, Divider, Label } from 'semantic-ui-react'
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+import TimeInput from 'react-time-input';
 
 
 class Activities extends React.Component {
+  state = {
+    startDate: moment()
+  }
+
+  handleChange = (date) => {
+    this.setState({
+      startDate: date
+    });
+  }
+
+
+
 
   render() {
     return(
-
-      <Header as='h3' textAlign='center'>Time</Header>
+      <Segment>
+      <Header as='h3' textAlign='center' >When are we hanging out?</Header>
+      <Label>Date</Label>
+       <DatePicker
+          selected={this.state.startDate}
+          onChange={this.handleChange}
+        />
+        <Label>Time</Label>
+        <Divider />
+        <Header>Location: </Header>
+        <Header>Date: </Header>
+        <Header>Description: </Header>
+        <Header>Name: </Header>
+        <Header>Indoor: </Header>
+        <Button>Save</Button>
+        <Button>Next</Button>
+      </Segment>
 
     );
   }
