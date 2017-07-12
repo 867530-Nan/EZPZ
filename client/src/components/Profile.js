2d6241050dd3b51ec5db567ec1121e806299c4e6
Stockton <stocktonhedges@gmail.com>
2017-07-12 15:24:25 -0600

    start of profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Segment, Icon, Button, Divider, Container, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import avatar from '../photodump/avatar1.jpg';
+import '../styles/profile.css';
 
 class Profile extends Component {
   render() {
     return(
-      <Header as='h1' textAlign='center'>
-        Page Not Found,
-        <Link to='/'> Home</Link>
-      </Header>
+      <Container>
+        <Segment>
+          <Header as='h1' textAlign='center'>
+            Welcome To Your Profile!
+          </Header>
+          <Divider />
+          <Segment compact>
+            <div>
+              <Image src={avatar} />
+              <Divider />
+              <div className='nameHere'>Name Here</div>
+            </div>
+          </Segment>
+        </Segment>
+      </Container>
     );
   }
 }
