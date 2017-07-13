482f931eb2215f65c3279a4f9b24211c8d213516
Stockton <stocktonhedges@gmail.com>
2017-07-13 10:18:30 -0600

    profile updates

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Segment, Icon, Button, Divider, Container, Image } from 'semantic-ui-react';
+import { Header, Segment, Icon, Button, Divider, Container, Image, Card } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import avatar from '../photodump/avatar1.jpg';
 import '../styles/profile.css';
@@ -7,21 +7,21 @@ import '../styles/profile.css';
 class Profile extends Component {
   render() {
     return(
-      <Container>
-        <Segment>
-          <Header as='h1' textAlign='center'>
-            Welcome To Your Profile!
-          </Header>
-          <Divider />
-          <Segment compact>
-            <div>
-              <Image src={avatar} />
-              <Divider />
-              <div className='nameHere'>Name Here</div>
-            </div>
-          </Segment>
-        </Segment>
-      </Container>
+      <div className='bigBox'>
+        <Header as='h1' textAlign='center'>
+        Welcome To Your Profile!
+        </Header>
+          <Container className='profContainer'>
+            <Segment compact className='avatar'>
+            <Image src={avatar} />
+            <Divider />
+            Your Name here
+            </Segment>
+            <Segment basic>
+              Content Here!
+            </Segment>
+          </Container>
+      </div>
     );
   }
 }
