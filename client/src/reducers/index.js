1a9e89e735c0082d471d0390bd6d23586db31417
Stockton <stocktonhedges@gmail.com>
2017-07-17 13:26:40 -0600

    added children reducer to index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,12 +3,14 @@ import user from './user'
 import flash from './flash'
 import activities from './activities'
 import photos from './photos';
+import children from './children';
 
 const rootReducer = combineReducers({
   user,
   flash,
   activities,
   photos,
+  children,
 })
 
 export default rootReducer
