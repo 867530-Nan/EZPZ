1af81fef33b2a44e21c98d300dec1b305fdd362c
Stockton <stocktonhedges@gmail.com>
2017-07-20 13:36:10 -0600

    404 image

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import '../styles/nomatch.css';
 
 class NoMatch extends Component {
   render() {
     return(
-      <Header as='h1' textAlign='center'>
-        Page Not Found,
-        <Link to='/'> Home</Link>
-      </Header>
+      <div className='nm-background'>
+        <Header as='h1' textAlign='center'>
+          Page Not Found,
+          <Link to='/'> Home</Link>
+        </Header>
+      </div>
     );
   }
 }
