3015527bb8de5d98051822d4d4b0ac9a09978ab0
Stockton <stocktonhedges@gmail.com>
2017-07-26 15:34:26 -0600

    attempt child

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { Header, Segment, Button, Divider, Container, Image, Card } from 'semantic-ui-react';
+import { Header,
+         Segment,
+         Button,
+         Divider,
+         Container,
+         Image,
+         Card,
+         Modal,
+        } from 'semantic-ui-react';
 import Dropzone from 'react-dropzone';
 import { Link } from 'react-router-dom';
 import { handleUpload } from '../actions/photos';
@@ -15,36 +23,54 @@ class Profile extends Component {
 
   render() {
 
+    const { children, name } = this.props.user;
+    const { age } = this.props.children;
+
     const images = this.props.user.image_url.map( image => {
       return(
-        <Segment>
-        <Image src={image} fluid />
+        <Segment basic className='actualPhoto'>
+          <Image src={image} fluid />
+          <Modal trigger={<Button>Enlarge Picture</Button>}>
+          <Modal.Header>Your Picture</Modal.Header>
+            <Modal.Content image>
+              <Image src={image} fluid />
+              <Modal.Description>
+                <Header>Default Profile Image</Header>
+                <p>We have found the following gravatar image associated with your e-mail address.</p>
+                <p>Is it okay to use this photo?</p>
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
         </Segment>
       )
     })
 
     return(
       <div>
-        <Dropzone
-          onDrop={ this.onDrop }
-          style={{ width: '100%', height: '100px', border: '1px dashed black'}}
-        >
-          <Header as='h4'>Try dropping some files or clicking here to upload!</Header>
-        </Dropzone>
-        <Segment basic>
-          <Header as='h2'>Family Photos!</Header>
-          <Divider />
-          <Segment.Group horizontal compact className='photos'>
-            { images }
-          </Segment.Group>
-        </Segment>
+        <Container>
+          <Header as='h1'>Welcome To Your Profile, {name}!</Header>
+          <Header as='h1'>Your Childs Age:</Header>
+          <Dropzone
+            onDrop={ this.onDrop }
+            style={{ width: '100%', height: '100px', border: '1px dashed black'}}
+          >
+            <Header as='h4'>Try dropping some files or clicking here to upload!</Header>
+          </Dropzone>
+          <Segment basic>
+            <Header as='h2'>Family Photos!</Header>
+            <Divider />
+            <Segment.Group horizontal compact className='photos'>
+              { images }
+            </Segment.Group>
+          </Segment>
+        </Container>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return { user: state.user };
+  return { user: state.user, children: state.children };
 }
 
 export default connect(mapStateToProps)(Profile);
